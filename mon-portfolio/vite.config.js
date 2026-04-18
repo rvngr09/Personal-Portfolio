360c952acf74f817d49293f39741fca9c8d96df3
rvngr09 <benallalamine2209@gmail.com>
2026-04-18 22:39:30 +0100

    changed config

--- mon-portfolio/vite.config.js
+++ mon-portfolio/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Personal-Portfolio/'
+  base: '/', // Make sure this is '/' not '/Personal-Portfolio/'
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
